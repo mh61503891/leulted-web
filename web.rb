7e283359db78617e2752bfc9200b3ec4185b6a87
Masayuki Higashino <m@msyk.info>
2013-12-15 04:58:32 +0900

    Add tmp_cache

--- web.rb
+++ web.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'open-uri'
 require 'nokogiri'
 require 'json'
+require 'tmp_cache'
 
 get '/' do
 	erb :index
@@ -15,32 +16,43 @@ get '/about.html' do
 	erb :about
 end
 
+
+
 get '/video.js' do
 	return not_found unless params[:id] || params[:url]
 	path = 'http://www.ted.com/talks/view/lang/en/id/' + params[:id] if params[:id]
 	path = params[:url] if params[:url]
 	return not_found unless path =~ /^http\:\/\/www.ted.com\/talks\//
 
-	meta = {}
-	doc = Nokogiri::HTML(open(path))
-	doc.xpath('//meta').each{ |e|
-		case e.attr('property')
-		when 'og:title', 'og:description', 'og:image', 'og:image', 'og:url', 'og:type'
-			meta['og'] ||= {}
-			meta['og'][e.attr('property').sub(/^og:/, '')] = e.attr('content') if e.attr('content')
-		when 'video:tag', 'video:duration', 'video:release_date'
-			meta['video'] ||= {}
-			meta['video'][e.attr('property').sub(/^video:/, '')] = e.attr('content') if e.attr('content')
+	get_javascript = lambda{ |url|
+		js = TmpCache.get(url)
+		if js
+			return js
 		else
-			# do noting
+			meta = {}
+			doc = Nokogiri::HTML(open(path))
+			doc.xpath('//meta').each{ |e|
+				case e.attr('property')
+				when 'og:title', 'og:description', 'og:image', 'og:image', 'og:url', 'og:type'
+					meta['og'] ||= {}
+					meta['og'][e.attr('property').sub(/^og:/, '')] = e.attr('content') if e.attr('content')
+				when 'video:tag', 'video:duration', 'video:release_date'
+					meta['video'] ||= {}
+					meta['video'][e.attr('property').sub(/^video:/, '')] = e.attr('content') if e.attr('content')
+				else
+					# do noting
+				end
+			}
+			js = ''
+			js << 'var video = {};'+ $/
+			js << "video['meta'] = " + JSON.generate(meta) + ';'+ $/
+			js << doc.xpath('//script').select{|e|e.text.include?('talkDetails')}.first.text + ';'+ $/
+			js << "video['details'] = talkDetails" + ';'+ $/
+			TmpCache.set(url, js, 1200)
+			return js
 		end
 	}
-	javascript = ''
-	javascript << 'var video = {};'+ $/
-	javascript << "video['meta'] = " + JSON.generate(meta) + ';'+ $/
-	javascript << doc.xpath('//script').select{|e|e.text.include?('talkDetails')}.first.text + ';'+ $/
-	javascript << "video['details'] = talkDetails" + ';'+ $/
-	javascript
+	return get_javascript.call(path)
 end
 
 get '/captions.json' do
